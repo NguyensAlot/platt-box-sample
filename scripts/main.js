436c8f5e1a6bdb932f199cca64d4dcc0be6f7533
Anthony <NguyenAnthony2014@yahoo.com>
2016-10-29 14:27:55 -0700

    added some functionality to the notification alert

--- scripts/main.js
+++ scripts/main.js
@@ -5,14 +5,28 @@
 
 
 $(document).ready(function() {
+    addPopover();
+    addSideNavHighlight();
+    startNotificationTimer();
+});
+
+function addPopover() {
     // add popover
     $('[data-toggle="popover"]').popover();
+}
 
+function addSideNavHighlight() {
     // side nav highlight
     $('.side-nav-text').click(function(e) {
         $('li').removeClass('side-nav-active');
         $(this).toggleClass('side-nav-active');
     });
+}
 
-});
-
+function startNotificationTimer() {
+    var counter = 0;
+    var alertNum = document.getElementById('notification-alert');
+    setInterval(function() {
+        alertNum.innerHTML = (++counter % 1000).toString();
+    }, 3000);
+}
